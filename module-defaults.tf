a6f451d24cb273826faf101ddaca4789c6efbdd3
junior <junior@users.noreply.github.com>
2022-09-26 22:34:13 -0500

    titles
    Signed-off-by: junior <junior@users.noreply.github.com>

--- module-defaults.tf
+++ module-defaults.tf
@@ -2,18 +2,7 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 # 
 
-variable "app_name" {
-  default     = "K8s App"
-  description = "Application name. Will be used as prefix to identify resources, such as OKE, VCN, ATP, and others"
-}
-variable "tag_values" {
-  type = map(any)
-  default = { "freeformTags" = {
-    "Environment" = "Development",  # e.g.: Demo, Sandbox, Development, QA, Stage, ...
-    "DeploymentType" = "generic" }, # e.g.: App Type 1, App Type 2, Red, Purple, ...
-  "definedTags" = {} }
-  description = "Use Tagging to add metadata to resources. All resources created by this stack will be tagged with the selected tag values."
-}
+# File Version: 0.7.0
 
 # Locals
 locals {
@@ -31,8 +20,4 @@ locals {
 resource "random_string" "deploy_id" {
   length  = 4
   special = false
-}
-
-output "deploy_id" {
-  value = local.deploy_id
 }
