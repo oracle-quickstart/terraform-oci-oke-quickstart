2236f1ce3dd052a63b7a96a64cf2ec7681f6337c
junior <junior@users.noreply.github.com>
2022-11-29 23:18:49 -0600

    variables updated to hold all variables
    Signed-off-by: junior <junior@users.noreply.github.com>

--- variables.tf
+++ variables.tf
@@ -1,4 +1,10 @@
-# OCI Provider
+# Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+# 
+
+################################################################################
+# OCI Provider Variables
+################################################################################
 variable "tenancy_ocid" {}
 variable "compartment_ocid" {}
 variable "region" {}
@@ -12,9 +18,9 @@ variable "private_key_path" {
   default = ""
 }
 
-
-# App defaults
-
+################################################################################
+# App Name to identify deployment. Used for naming resources.
+################################################################################
 variable "app_name" {
   default     = "K8s App"
   description = "Application name. Will be used as prefix to identify resources, such as OKE, VCN, ATP, and others"
@@ -26,4 +32,32 @@ variable "tag_values" {
     "DeploymentType" = "generic" }, # e.g.: App Type 1, App Type 2, Red, Purple, ...
   "definedTags" = {} }
   description = "Use Tagging to add metadata to resources. All resources created by this stack will be tagged with the selected tag values."
+}
+
+################################################################################
+# OCI Network - VCN Variables
+################################################################################
+variable "create_new_vcn" {
+  default     = true
+  description = "Creates a new Virtual Cloud Network (VCN). If false, the VCN must be provided in the variable 'existent_vcn_ocid'."
+}
+variable "existent_vcn_ocid" {
+  default     = ""
+  description = "Using existent Virtual Cloud Network (VCN) OCID."
+}
+variable "existent_vcn_compartment_ocid" {
+  default     = ""
+  description = "Compartment OCID for existent Virtual Cloud Network (VCN)."
+}
+variable "vcn_cidr_blocks" {
+  default     = "10.20.0.0/16"
+  description = "IPv4 CIDR Blocks for the Virtual Cloud Network (VCN). If use more than one block, separate them with comma. e.g.: 10.20.0.0/16,10.80.0.0/16. If you plan to peer this VCN with another VCN, the VCNs must not have overlapping CIDRs."
+}
+variable "is_ipv6enabled" {
+  default     = false
+  description = "Whether IPv6 is enabled for the Virtual Cloud Network (VCN)."
+}
+variable "ipv6private_cidr_blocks" {
+  default     = []
+  description = "The list of one or more ULA or Private IPv6 CIDR blocks for the Virtual Cloud Network (VCN)."
 }
