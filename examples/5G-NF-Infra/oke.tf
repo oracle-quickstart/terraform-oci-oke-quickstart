3a4f80eb71df99a5a9463a21721a5edf0d333207
junior <junior@users.noreply.github.com>
2022-12-11 19:23:20 -0600

    5G NF Infra example: sctp seclist
    Signed-off-by: junior <junior@users.noreply.github.com>

--- examples/5G-NF-Infra/oke.tf
+++ examples/5G-NF-Infra/oke.tf
@@ -26,14 +26,17 @@ module "oke-quickstart" {
   extra_subnets        = local.extra_subnets
 
   # OKE Node Pool 1 arguments
-  node_pool_cni_type_1                                   = "OCI_VCN_IP_NATIVE" # Use "FLANNEL_OVERLAY" for overlay network or "OCI_VCN_IP_NATIVE" for VCN Native PODs Network. If the node pool 1 uses the OCI_VCN_IP_NATIVE, the cluster will also be configured with same cni
-  cluster_autoscaler_enabled                             = true
-  node_pool_name_1                                       = "pool1"
-  node_pool_initial_num_worker_nodes_1                   = var.node_pool_initial_num_worker_nodes_1 # Minimum number of nodes in the node pool
-  node_pool_max_num_worker_nodes_1                       = var.node_pool_max_num_worker_nodes_1     # Maximum number of nodes in the node pool
-  node_pool_instance_shape_1                             = var.node_pool_instance_shape_1
-  extra_security_list_name_for_nodes                     = "5g_for_pods_security_list"
-  extra_security_list_name_for_vcn_native_pod_networking = "5g_for_pods_security_list"
+  node_pool_cni_type_1                 = "OCI_VCN_IP_NATIVE" # Use "FLANNEL_OVERLAY" for overlay network or "OCI_VCN_IP_NATIVE" for VCN Native PODs Network. If the node pool 1 uses the OCI_VCN_IP_NATIVE, the cluster will also be configured with same cni
+  cluster_autoscaler_enabled           = true
+  node_pool_name_1                     = "pool1"
+  node_pool_initial_num_worker_nodes_1 = var.node_pool_initial_num_worker_nodes_1 # Minimum number of nodes in the node pool
+  node_pool_max_num_worker_nodes_1     = var.node_pool_max_num_worker_nodes_1     # Maximum number of nodes in the node pool
+  node_pool_instance_shape_1           = var.node_pool_instance_shape_1
+  extra_initial_node_labels_1          = [{ key = "cnf", value = "amf01" }] # Extra initial node labels for node pool 1. Example: "[{ key = "app.something/key1", value = "value1" }]"
+
+  # Extra Security Lists
+  extra_security_list_name_for_nodes                     = "5g_for_pods_security_list" # ["5g_for_pods_security_list", "5g_sctp_security_list"]
+  extra_security_list_name_for_vcn_native_pod_networking = "5g_for_pods_security_list" # ["5g_for_pods_security_list", "5g_sctp_security_list"]
 
   # Cluster Tools
   # ingress_nginx_enabled = true
