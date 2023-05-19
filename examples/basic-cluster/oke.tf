b2393c2a66359c0170ff0ca80dc4de5134bedf2d
junior <junior@users.noreply.github.com>
2023-05-19 16:57:34 -0500

    examples: basic cluster update
    Signed-off-by: junior <junior@users.noreply.github.com>

--- examples/basic-cluster/oke.tf
+++ examples/basic-cluster/oke.tf
@@ -5,6 +5,11 @@
 module "oke-quickstart" {
   source = "github.com/oracle-quickstart/terraform-oci-oke-quickstart?ref=0.9.0"
 
+  providers = {
+    oci             = oci
+    oci.home_region = oci.home_region
+  }
+
   # Oracle Cloud Infrastructure Tenancy and Compartment OCID
   tenancy_ocid     = var.tenancy_ocid
   compartment_ocid = var.compartment_ocid
@@ -23,6 +28,7 @@ module "oke-quickstart" {
   node_pool_initial_num_worker_nodes_1 = 3                                                                       # Minimum number of nodes in the node pool
   node_pool_max_num_worker_nodes_1     = 10                                                                      # Maximum number of nodes in the node pool
   node_pool_instance_shape_1           = { "instanceShape" = "VM.Standard.E4.Flex", "ocpus" = 2, "memory" = 64 } # If not using a Flex shape, ocpus and memory are ignored
+  node_pool_boot_volume_size_in_gbs_1  = 120
 
   # VCN for OKE arguments
   vcn_cidr_blocks = "10.22.0.0/16"
